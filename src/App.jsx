f70be2a05656294bd122de30cb23a7d81432bc71
Jorge Romero <79546858+jorgeromero5055@users.noreply.github.com>
2026-07-04 12:55:59 -0400

    Add custom AR button

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,25 @@ function App() {
       ar
       ar-modes="webxr scene-viewer quick-look"
       style={{ width: "100vw", height: "100vh" }}
-    ></model-viewer>
+    >
+      <button
+        slot="ar-button"
+        style={{
+          position: "absolute",
+          top: "16px",
+          left: "50%",
+          transform: "translateX(-50%)",
+          padding: "12px 20px",
+          borderRadius: "999px",
+          border: "none",
+          background: "black",
+          color: "white",
+          fontSize: "16px",
+        }}
+      >
+        View in your room 👋
+      </button>
+    </model-viewer>
   );
 }
 
